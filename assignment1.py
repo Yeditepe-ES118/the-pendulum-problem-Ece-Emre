aec2ee317e7f43ef44853a9710dfe6dfec174495
Ece Emre <ece.emre2@std.yeditepe.edu.tr>
2025-11-01 11:05:45 +0300

    Add assignment1.py with find_period function

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+import math
+
+def find_period(L0, L1):
+    g = 9.81
+    T0 = 2 * math.pi * math.sqrt(L0 / g)
+    T1 = 2 * math.pi * math.sqrt(L1 / g)
+    
+    for L in range(L0, L1 + 1):
+        T = 2 * math.pi * math.sqrt(L / g)
+        print(f"When L = {float(L):.1f} m, T = {T:.1f} s")
+    
+    return T0, T1
